8bc26c5efafc12b7490c5a35ff25a4389bdc6047
Habib ul Rehman <mhabibulrehman@gmail.com>
2019-12-22 15:27:25 +0500

    Add project files.

---
+++ Car Evaluation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Car_Evaluation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
